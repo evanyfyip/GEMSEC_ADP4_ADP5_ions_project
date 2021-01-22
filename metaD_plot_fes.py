7100152dc8905bb8c506b12b81e0c800476e9fb1
Evan Yip <eyfy@uw.edu>
2021-01-22 02:08:41 -0800

    Add files via upload

--- metaD_plot_fes.py
+++ metaD_plot_fes.py
@@ -9,62 +9,59 @@ Created on Thurs 9/3/2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import argparse
-
-def main():
-    # Parsing the arguments
-    global parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument('Input .dat file')
-    plt.rcParams.update({'font.size': 12})
-    ##### DIST CV
-    dist = pd.DataFrame(np.loadtxt('WT_dist_fes.txt')) # input file
-    dist.columns = ['cv', 'fe'] # renaming columns
-    dist.fe = dist.fe * 0.239006 # kJ/mol to kCal/mol
-
-    # plot
-    fig = plt.figure()
-    plt.grid()
-    plt.plot(dist.cv, dist.fe)
-    plt.title('Free Energy Surface - Dist CV')
-    plt.ylabel('Free Energy (kCal/mol)')
-    plt.xlabel('Dist to graphene (nm)')
-    plt.show()
-    fig.savefig('wt_fes_dist.png')
 
 
-    ##### RGYR CV
-    gyr = pd.DataFrame(np.loadtxt('pep_gyr_fes.txt'))
-    gyr.columns = ['cv', 'fe', 'der_gyr']
-    gyr.fe = gyr.fe * 0.239006 # kJ/mol to kCal/mol
+plt.rcParams.update({'font.size': 12})
+##### DIST CV
+gyr = pd.DataFrame(np.loadtxt('pep_gyr_fes.txt')) # input file
+gyr.columns = ['cv', 'fe', 'der_gyr'] # renaming columns
+gyr.fe = gyr.fe * 0.239006 # kJ/mol to kCal/mol
 
-    fig = plt.figure()
-    plt.grid()
-    plt.plot(gyr.cv, gyr.der_gyr)
-    plt.title('Free Energy Surface - RGYR CV')
-    plt.ylabel('Free Energy (kCal/mol)')
-    plt.xlabel('Radius of Gyration (nm)')
-    plt.show()
-    fig.savefig('wt_fes_gyr.png')
+# plot
+fig = plt.figure()
+plt.grid()
+plt.plot(gyr.cv, gyr.fe)
+# If you want the red dots for the lowest energy points in each well
+plt.plot([0.755105, 1.178071],[-62.745258, -35.3948646], "ro")
+plt.title('Free Energy Surface ADP4 - Gyr CV')
+plt.ylabel('Free Energy (kCal/mol)')
+plt.xlabel('Radius of Gyration (nm)')
+plt.show()
+fig.savefig('sADP5_fes_dist.png')
 
 
-    ##### CONVERGENCE
-    conv = pd.DataFrame(np.loadtxt('fes_conv_8A_100ns.txt'))
-    conv.columns = ['step', 'fe']
-    conv.fe = conv.fe * 0.239006 # kJ/mol to kCal/mol
+##### RGYR CV
+gyr = pd.DataFrame(np.loadtxt('pep_gyr_fes.txt'))
+gyr.columns = ['cv', 'fe', 'der_gyr']
+gyr.fe = gyr.fe * 0.239006 # kJ/mol to kCal/mol
 
-    # taking differences in convergence
-    a = conv.fe[:-1]
-    b = conv.fe[1:]
-    a.index = b.index
-    diff = a - b
-    diff.index = diff.index/2
+fig = plt.figure()
+plt.grid()
+plt.plot(gyr.cv, gyr.der_gyr)
+plt.title('Free Energy Surface - RGYR CV')
+plt.ylabel('Free Energy (kCal/mol)')
+plt.xlabel('Radius of Gyration (nm)')
+plt.show()
+fig.savefig('wt_fes_gyr.png')
 
-    fig = plt.figure()
-    plt.grid()
-    plt.plot(diff)
-    plt.title('Free Energy Convergence of WT-GrBP5-8A')
-    plt.ylabel('Free Energy (kCal/mol)')
-    plt.xlabel('Time (ns)')
-    plt.show()
-    fig.savefig('fes_conv_wt8a_100ns.png')
+
+##### CONVERGENCE
+conv = pd.DataFrame(np.loadtxt('fes_conv_8A_100ns.txt'))
+conv.columns = ['step', 'fe']
+conv.fe = conv.fe * 0.239006 # kJ/mol to kCal/mol
+
+# taking differences in convergence
+a = conv.fe[:-1]
+b = conv.fe[1:]
+a.index = b.index
+diff = a - b
+diff.index = diff.index/2
+
+fig = plt.figure()
+plt.grid()
+plt.plot(diff)
+plt.title('Free Energy Convergence of WT-GrBP5-8A')
+plt.ylabel('Free Energy (kCal/mol)')
+plt.xlabel('Time (ns)')
+plt.show()
+fig.savefig('fes_conv_wt8a_100ns.png')
